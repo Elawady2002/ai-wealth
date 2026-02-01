315740a5212caaa70765b31e0cbf7a25b0f6631d
Abdallah <abdullahelawady68@gmail.com>
2026-02-01 13:44:59 +0200

    Updates

--- _next/static/I-Db-HpOKrQklD52kdxvT/_buildManifest.js
+++ _next/static/I-Db-HpOKrQklD52kdxvT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ai-wealth//_next/:path+",
+        "destination": "/ai-wealth/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
